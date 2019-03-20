2d6aae2bcea5df092f056783f60ef436001e2140
qbcs <qbcs@9fbank.cc>
2019-03-20 18:42:48 +0800

    修正文件名，增加题目描述，方便回头复习

--- leetcode_13罗马数字转整数.py
+++ leetcode_13罗马数字转整数.py
@@ -4,6 +4,16 @@
  Created on 2019/3/12
  @author: luomashuzi
  desc:
+ 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
 '''
 
 class Solution(object):
---
+++ leetcode_7整数反转.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+ Created on 2019/3/12
+ @author: luomashuzi
+ desc:
+ 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+示例 1:
+
+输入: 123
+输出: 321
+ 示例 2:
+
+输入: -123
+输出: -321
+示例 3:
+
+输入: 120
+输出: 21
+'''
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x==0:
+            return 0
+        str_x = str(x)
+        x = ''
+        if str_x[0] == '-':
+            x += '-'
+        x += str_x[len(str_x)-1::-1].lstrip("0").rstrip("-")
+        x = int(x)
+        if -2**31<x<2**31-1:
+            return x
+        return 0
+
+
+if __name__ == '__main__':
+    a=Solution()
+    print(a.reverse(-123))
